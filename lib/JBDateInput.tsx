29d0b9281c5cffeb120f846f558c4dc740f7a29f
javadbat <javadbat@gmail.com>
2024-06-30 19:08:38 +0330

    fix import

--- lib/JBDateInput.tsx
+++ lib/JBDateInput.tsx
@@ -55,132 +55,132 @@ export type JBDateInputProps = {
 }
 
 export const JBDateInput = React.forwardRef((props: JBDateInputProps, ref) => {
-    const element = useRef<JBDateInputWebComponent>(null);
-    const [refChangeCount, refChangeCountSetter] = useState(0);
-    const onFormatChangeCallBackQueueRef = useRef<(()=>void)[]>([]);
-    useImperativeHandle(
-        ref,
-        () => (element ? element.current : {}),
-        [element],
-    );
-    useEffect(() => {
-        refChangeCountSetter(refChangeCount + 1);
-    }, [element.current]);
-    const onchange = useCallback((e:JBDateInputEventType<Event>) => {
-        if (props.onChange) {
-            props.onChange(e);
-        }
-    }, [props.onChange]);
-    const onKeyup = useCallback((e:JBDateInputEventType<KeyboardEvent>) => {
-        if (props.onKeyup) {
-            props.onKeyup(e);
-        }
-    }, [props.onKeyup]);
-    const onSelect = useCallback((e:JBDateInputEventType<CustomEvent>) => {
-        if (props.onSelect) {
-            props.onSelect(e);
-        }
-    }, [props.onSelect]);
-    useEvent(element.current, 'change', onchange, true);
-    useEvent(element.current, 'keyup', onKeyup, true);
-    useEvent(element.current, 'select', onSelect, true);
-    useEffect(() => {
-        if (props.format) {
-            if (props.format !== element.current?.valueFormat) {
-                element.current?.setAttribute('format', props.format);
-            }
-            if (onFormatChangeCallBackQueueRef.current.length > 0) {
-                onFormatChangeCallBackQueueRef.current.forEach((callBack:()=>void) => {
-                    callBack();
-                });
-                onFormatChangeCallBackQueueRef.current = [];
-            }
+  const element = useRef<JBDateInputWebComponent>(null);
+  const [refChangeCount, refChangeCountSetter] = useState(0);
+  const onFormatChangeCallBackQueueRef = useRef<(()=>void)[]>([]);
+  useImperativeHandle(
+    ref,
+    () => (element ? element.current : {}),
+    [element],
+  );
+  useEffect(() => {
+    refChangeCountSetter(refChangeCount + 1);
+  }, [element.current]);
+  const onchange = useCallback((e:JBDateInputEventType<Event>) => {
+    if (props.onChange) {
+      props.onChange(e);
+    }
+  }, [props.onChange]);
+  const onKeyup = useCallback((e:JBDateInputEventType<KeyboardEvent>) => {
+    if (props.onKeyup) {
+      props.onKeyup(e);
+    }
+  }, [props.onKeyup]);
+  const onSelect = useCallback((e:JBDateInputEventType<CustomEvent>) => {
+    if (props.onSelect) {
+      props.onSelect(e);
+    }
+  }, [props.onSelect]);
+  useEvent(element.current, 'change', onchange, true);
+  useEvent(element.current, 'keyup', onKeyup, true);
+  useEvent(element.current, 'select', onSelect, true);
+  useEffect(() => {
+    if (props.format) {
+      if (props.format !== element.current?.valueFormat) {
+        element.current?.setAttribute('format', props.format);
+      }
+      if (onFormatChangeCallBackQueueRef.current.length > 0) {
+        onFormatChangeCallBackQueueRef.current.forEach((callBack:()=>void) => {
+          callBack();
+        });
+        onFormatChangeCallBackQueueRef.current = [];
+      }
 
-        }
-    }, [props.format]);
-    useEffect(() => {
-        if (props.max) {
-            if (props.format && props.format !== element.current?.valueFormat) {
-                onFormatChangeCallBackQueueRef.current.push(() => {
-                    if(props.max){element.current?.setMaxDate(props.max);}
-                });
-            } else {
-                element.current?.setMaxDate(props.max);
-            }
-        }
+    }
+  }, [props.format]);
+  useEffect(() => {
+    if (props.max) {
+      if (props.format && props.format !== element.current?.valueFormat) {
+        onFormatChangeCallBackQueueRef.current.push(() => {
+          if(props.max){element.current?.setMaxDate(props.max);}
+        });
+      } else {
+        element.current?.setMaxDate(props.max);
+      }
+    }
 
-    }, [props.max]);
-    useEffect(() => {
-        if (props.min) {
-            if (props.format && props.format !== element.current?.valueFormat) {
-                onFormatChangeCallBackQueueRef.current.push(() => {
-                    props.min && element.current?.setMinDate(props.min);
-                });
-            } else {
-                element.current?.setMinDate(props.min);
-            }
-        }
-    }, [props.min]);
-    useEffect(() => {
-        if (element.current && props.value) {
-            element.current.value = props.value;
-        }
-    }, [props.value]);
-    useEffect(() => {
-        if (element.current && Array.isArray(props.jalaliMonthList)) {
-            element.current.setMonthList("JALALI",props.jalaliMonthList);
-        }
-    }, [props.jalaliMonthList]);
-    useEffect(() => {
-        if (element.current && Array.isArray(props.gregorianMonthList)) {
-            element.current.setMonthList("GREGORIAN",props.gregorianMonthList);
-        }
-    }, [props.gregorianMonthList]);
-    useEffect(() => {
-        if (element.current && props.placeholder !== undefined) {
-            element.current.placeholder = props.placeholder;
-        }
-    }, [props.placeholder]);
-    useEffect(() => {
-        if (element.current) {
-            if(typeof props.style == "string"){
-                element.current.setAttribute("style",props.style);
-            }
-        }
-    }, [props.style]);
-    useEffect(() => {
-        if (element.current && Array.isArray(props.validationList)) {
-            element.current.validationList = props.validationList;
-        }
-    }, [props.validationList]);
-    useEffect(() => {
-        if (element.current && props.direction) {
-            element.current.setAttribute('direction', props.direction);
-        }
-    },[props.direction]);
-    useEffect(() => {
-        if (props.required) {
-            element.current?.setAttribute('required', "true");
-        } else {
-            element.current?.removeAttribute('required');
-        }
-    }, [props.required]);
-    useEffect(() => {
-        if (typeof props.calendarDefaultDateView == "object" && props.calendarDefaultDateView.year && props.calendarDefaultDateView.month) {
-            element.current?.setCalendarDefaultDateView(props.calendarDefaultDateView.year, props.calendarDefaultDateView.month, props.calendarDefaultDateView.dateType);
-        }
-    }, [props.calendarDefaultDateView]);
-    useEffect(() => {
-        if (props.usePersianNumber) {
-            element.current?.setAttribute('use-persian-number', 'true');
-        } else {
-            element.current?.removeAttribute('use-persian-number');
-        }
-    }, [props.usePersianNumber]);
-    return (
-        <jb-date-input class={props.className ? props.className : ""} name={props.name} label={props.label} value-type={props.valueType ? props.valueType : 'GREGORIAN'} ref={element} input-type={props.inputType ? props.inputType : 'JALALI'}>
-            {props.children}
-        </jb-date-input>
-    );
+  }, [props.max]);
+  useEffect(() => {
+    if (props.min) {
+      if (props.format && props.format !== element.current?.valueFormat) {
+        onFormatChangeCallBackQueueRef.current.push(() => {
+          props.min && element.current?.setMinDate(props.min);
+        });
+      } else {
+        element.current?.setMinDate(props.min);
+      }
+    }
+  }, [props.min]);
+  useEffect(() => {
+    if (element.current && props.value) {
+      element.current.value = props.value;
+    }
+  }, [props.value]);
+  useEffect(() => {
+    if (element.current && Array.isArray(props.jalaliMonthList)) {
+      element.current.setMonthList("JALALI",props.jalaliMonthList);
+    }
+  }, [props.jalaliMonthList]);
+  useEffect(() => {
+    if (element.current && Array.isArray(props.gregorianMonthList)) {
+      element.current.setMonthList("GREGORIAN",props.gregorianMonthList);
+    }
+  }, [props.gregorianMonthList]);
+  useEffect(() => {
+    if (element.current && props.placeholder !== undefined) {
+      element.current.placeholder = props.placeholder;
+    }
+  }, [props.placeholder]);
+  useEffect(() => {
+    if (element.current) {
+      if(typeof props.style == "string"){
+        element.current.setAttribute("style",props.style);
+      }
+    }
+  }, [props.style]);
+  useEffect(() => {
+    if (element.current && Array.isArray(props.validationList)) {
+      element.current.validationList = props.validationList;
+    }
+  }, [props.validationList]);
+  useEffect(() => {
+    if (element.current && props.direction) {
+      element.current.setAttribute('direction', props.direction);
+    }
+  },[props.direction]);
+  useEffect(() => {
+    if (props.required) {
+      element.current?.setAttribute('required', "true");
+    } else {
+      element.current?.removeAttribute('required');
+    }
+  }, [props.required]);
+  useEffect(() => {
+    if (typeof props.calendarDefaultDateView == "object" && props.calendarDefaultDateView.year && props.calendarDefaultDateView.month) {
+      element.current?.setCalendarDefaultDateView(props.calendarDefaultDateView.year, props.calendarDefaultDateView.month, props.calendarDefaultDateView.dateType);
+    }
+  }, [props.calendarDefaultDateView]);
+  useEffect(() => {
+    if (props.usePersianNumber) {
+      element.current?.setAttribute('use-persian-number', 'true');
+    } else {
+      element.current?.removeAttribute('use-persian-number');
+    }
+  }, [props.usePersianNumber]);
+  return (
+    <jb-date-input class={props.className ? props.className : ""} name={props.name} label={props.label} value-type={props.valueType ? props.valueType : 'GREGORIAN'} ref={element} input-type={props.inputType ? props.inputType : 'JALALI'}>
+      {props.children}
+    </jb-date-input>
+  );
 });
 JBDateInput.displayName = "JBDateInput";
